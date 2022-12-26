e81fec3af95c97c82bd2081ed9ae20529c685f43
Marcos Bryan <marcosbryan1515@gmai.com>
2022-12-26 14:58:40 -0400

    test

--- page-home.php
+++ page-home.php
@@ -18,6 +18,8 @@ $builder = get_post_meta( $post->ID, 'tipi_builder_active', true );
 
 <div id="primary" class="content-area">
 
+<div class="qqmerda"></div>
+
 	<div class="home-wrap">
 
 		<!-- Anúncio 1 -->
