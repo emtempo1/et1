c345148cb756d0a5753965532a26e46d9d54dee6
Marcos Bryan <marcosbryan1515@gmail.com>
2023-01-24 16:24:39 -0300

    Personalização preliminar do login

--- functions.php
+++ functions.php
@@ -389,12 +389,6 @@ add_filter( 'zeen_web_vitals', '__return_true' );
 // Personaliza o icone na tela de login 
 function custom_login_logo() {
     echo '<style>
-        body.login {
-            background-image: url(http://localhost/emtempo1/wp-content/uploads/2023/01/suspension-bridge-959853.jpg);
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
-        }
         h1 a {border-radius: 30%;}
     </style>';
 }
